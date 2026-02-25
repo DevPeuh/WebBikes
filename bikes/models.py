bd3a776a5f10a96c4413666ac757c586fb3ef522
DevPeuh <phgdealmeida@gmail.com>
2026-02-25 18:16:10 -0300

    add signals events

--- bikes/models.py
+++ bikes/models.py
@@ -8,7 +8,7 @@ class Brand(models.Model):
     def __str__(self): # Retorna o nome da marca
         return self.name
 
-class Bike(models.Model):
+class Bike(models.Model): # models.model serve para criar uma tabela no banco de dados, cada classe representa uma tabela e cada atributo representa uma coluna da tabela
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=100) # Modelo do moto
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='bike_brands') # vai fazer a relação com a tabela Brand, ter proteção para não exluir a marca.
@@ -16,9 +16,21 @@ class Bike(models.Model):
     model_year = models.IntegerField(blank=True, null=True) # Ano do modelo podendo ser nulo
     plate = models.CharField(max_length=10, blank=True, null=True) # Placa da moto podendo ser nulo
     value = models.FloatField(blank=True, null=True) # Valor da moto podendo ser nulo
-    photo = models.ImageField(upload_to='bikes/', blank=False, null=False) # Foto da moto salva no diretório bikes
+    photo = models.ImageField(upload_to='bikes/', blank=True, null=True) # Foto da moto salva no diretório bikes
     
 
     def __str__(self): # Retorna o modelo da moto
         return self.model 
     
+
+class BikeInventory(models.Model):
+    bikes_count = models.IntegerField() # Quantidade de motos no estoque
+    bikes_value = models.FloatField() # Valor total das motos no estoque
+    created_at = models.DateTimeField(auto_now_add=True) # Data de criação do registro
+    updated_at = models.DateTimeField(auto_now=True) # Data de atualização do registro
+
+    class Meta:
+        ordering = ['-created_at'] # Ordena os registros pela data de criação, do mais recente para o mais antigo
+
+    def __str__(self):
+        return f'Bikes - {self.bikes_count} - {self.bikes_value}'
--- bikes/signals.py
+++ bikes/signals.py
@@ -1,22 +1,18 @@
+# signals.py é um arquivo onde podemos definir funções que serão executadas quando determinados eventos ocorrerem em nossos modelos. Por exemplo, podemos definir uma função que será executada toda vez que um objeto do modelo Bike for salvo ou deletado. Isso é útil para realizar ações adicionais, como enviar notificações, atualizar outros modelos relacionados, ou simplesmente para fins de logging.
+
 from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
 from django.dispatch import receiver # receiver é um decorador que serve para registrar a função como um receptor de sinais, ou seja, ela será chamada quando o sinal for emitido
 from bikes.models import Bike
 
-@receiver(pre_save, sender=Bike)
-def bike_pre_save(sender, instance, **kwargs): # sender serve para identificar qual modelo está sendo salvo, instance é a instância do modelo que está sendo salva e **kwargs serve para receber outros argumentos que possam ser passados
-    print(' PRE SAVE ')
 
-
-@receiver(post_save, sender=Bike)
-def bike_post_save(sender, instance, **kwargs):
-    print(' POST SAVE ')
-
-
-@receiver(pre_delete, sender=Bike)
-def bike_pre_delete(sender, instance, **kwargs):
-    print(' PRE DELETE ')
-
+@receiver(post_save, sender=Bike) 
+def bike_post_save(sender, instance, **kwargs):# sender serve para identificar qual modelo está sendo salvo, instance é a instância do modelo que está sendo salva e **kwargs serve para receber outros argumentos que possam ser passados
+    bike_count = Bike.objects.all().count()
+    bike_value = Bike.objects.aggregate(
+        total_value = sum('value')
+    ) # a função aggregate é usada para realizar operações de agregação, como contar o número de objetos ou calcular a soma de um campo específico.
 
 @receiver(post_delete, sender=Bike)
 def bike_post_delete(sender, instance, **kwargs):
     print(' POST DELETE ')
+    print(instance)
