531ac6975ba00eda44a758d25f3b242ed09f8a36
DevPeuh <phgdealmeida@gmail.com>
2026-03-04 17:24:26 -0300

    def inventory

--- bikes/signals.py
+++ bikes/signals.py
@@ -2,17 +2,24 @@
 
 from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
 from django.dispatch import receiver # receiver é um decorador que serve para registrar a função como um receptor de sinais, ou seja, ela será chamada quando o sinal for emitido
-from bikes.models import Bike
+from bikes.models import Bike, BikeInventory
 
 
-@receiver(post_save, sender=Bike) 
-def bike_post_save(sender, instance, **kwargs):# sender serve para identificar qual modelo está sendo salvo, instance é a instância do modelo que está sendo salva e **kwargs serve para receber outros argumentos que possam ser passados
+def bike_inventory_update():
     bike_count = Bike.objects.all().count()
     bike_value = Bike.objects.aggregate(
         total_value = sum('value')
-    ) # a função aggregate é usada para realizar operações de agregação, como contar o número de objetos ou calcular a soma de um campo específico.
+    )['total_value'] # a função aggregate é usada para realizar operações de agregação, como contar o número de objetos ou calcular a soma de um campo específico.
+    BikeInventory.objects.create(
+        bike_count = bike_count,
+        bike_value = bike_value,
+    )
+
+@receiver(post_save, sender=Bike) 
+def bike_post_save(sender, instance, **kwargs):# sender serve para identificar qual modelo está sendo salvo, instance é a instância do modelo que está sendo salva e **kwargs serve para receber outros argumentos que possam ser passados
+    bike_inventory_update()
 
 @receiver(post_delete, sender=Bike)
 def bike_post_delete(sender, instance, **kwargs):
-    print(' POST DELETE ')
-    print(instance)
+    bike_inventory_update()
+    
